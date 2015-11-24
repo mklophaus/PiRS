7146cb46e89ba1593ce51f9eeab30d33b9536739
Melissa Wilcox <melissa@emdubb.co>
2015-11-23 16:00:22 -0800

    add login and circles templates

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
 console.log('JS loaded!');
+
+var login     = _.template($('#login').html());
+var circles   = _.template($('#circles').html());
+
+var LoginView = function() {
+  var self = this;
+
+};
+
+function LoginView.prototype.render() {
+
+};
