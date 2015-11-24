142134258d4b64092e59e36ba103fec344ae0e3f
Melissa Wilcox <melissa@emdubb.co>
2015-11-23 17:14:14 -0800

    add login template

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,17 @@
 console.log('JS loaded!');
 
-var login     = _.template($('#login').html());
+$(document).ready(function() {
+
+
+var login     = _.template($('#login-template').html());
 var circles   = _.template($('#circles').html());
 
-var LoginView = function() {
-  var self = this;
+var $destination,
+    $templateEl;
 
-};
+$destination = $('#login');
 
-function LoginView.prototype.render() {
 
-};
+$destination.append(login);
+
+});
