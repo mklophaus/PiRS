4aafb30f7018c6bd1381c3fbdb285977f7b9b433
J T <njthood@yahoo.com>
2015-11-27 21:59:25 -0800

    callback route added

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ module.exports = function(passport) {
   passport.use(new SpotifyStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/callback"
+    callbackURL: "https://piradio.herokuapp.com/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOne({ 'spotifyId': profile.id }, function(err, user) {
