c5e6d4c8d3c67be360182e34c9bd03c9e63677e2
J T <njthood@yahoo.com>
2015-11-27 20:13:56 -0800

    delete piradio folder

--- config/database.js
+++ config/database.js
@@ -1,7 +1,28 @@
+// var mongoose = require('mongoose');
+
+// require('dotenv').load()
+
+// mongoose.connect(process.env.DATABASE_URL);
+
+// module.exports = mongoose;
+
+
 var mongoose = require('mongoose');
 
-require('dotenv').load()
+var env = require('./environment');
 
-mongoose.connect(process.env.DATABASE_URL);
+// Use different database URIs based on whether an env var exists.
+var dbUri = env.DATABASE_URL ||
+            'mongodb://localhost/' + env.SAFE_TITLE;
+
+if (!env.DATABASE_URL) {
+  // check that MongoD is running...
+  require('net').connect(27017, 'localhost').on('error', function() {
+    console.log("YOU MUST BOW BEFORE THE MONGOD FIRST, MORTAL!");
+    process.exit(0);
+  });
+}
+
+mongoose.connect(dbUri);
 
 module.exports = mongoose;
