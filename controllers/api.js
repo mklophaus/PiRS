2c23dafddb009a1c819dd6e89ef1fc9734359b64
Melissa Wilcox <melissa@emdubb.co>
2015-11-29 12:09:45 -0800

    work on saving circles

--- controllers/api.js
+++ controllers/api.js
@@ -4,25 +4,6 @@ var spotify = require('../config/spotifyApiHelper');
 var locus = require('locus');
 var async = require('async');
 
-var addCircleUsers = function(req, res, done){
-  User.findOne(req.body, function(err, user) {
-    if (err) return done(err);
-    if (user) {
-      res.json(user);
-    } else {
-      var newUser = new User({
-        spotifyId: req.body.spotifyId,
-        circles: []
-      });
-      newUser.save(function(err, user) {
-        if (err) return done(err);
-        res.json(user);
-      });
-    }
-  });
-};
-
-
 var indexCircle = function(req, res) {
   Circle.find({}, function(err, records) {
     res.json(records);
@@ -30,46 +11,67 @@ var indexCircle = function(req, res) {
 };
 
 var createCircle = function(req, res, done) {
-  var circle = new Circle(req.body);
-  circle.creator = req.user._id;
-  circle.users.push(req.user._id);
-  var userArray = JSON.parse(req.body.users);
-  async.each(userArray, function(user){
-    circle.users.push(user);
-  });
-    circle.save(function(err){
-      if (err) return done(err);
-      console.log("that shit SAVED.");
-    });
-
-  // var circle = Circle.create(req.body, function(err, newCircle){
-  //   var userArray = JSON.parse(req.body.users);
-  //   userArray.forEach(function(user){
-  //   eval(locus);
-  //     newCircle.users.push(user)
-  //   });
-  //   circle.creator = req.user._id;
-  //   circle.users.push(req.user._id);
-    // async.each(userArray, function(user){
-    //   circle.users.push(user);
-    // });
-
-
-
-
-  async.each(circle.users, function(user){
-    User.findOne({ '_id': user }, function(err, user) {
-      if (err) return done(err);
-      if (user) {
-        foundUser = user;
-        foundUser.circles.push(circle);
-        foundUser.save(function(err){
+  var circleUsersString = JSON.parse(req.body.users);
+  var circleUsers = [req.user._id];
+  var circle;
+  async.series([
+    function (callback) {
+      circle = new Circle({
+        creator: req.user._id,
+        users: circleUsers,
+        title: req.body.title
+      });
+      circle.users = circleUsers;
+      // return circle
+      callback(null, 'one');
+    },
+    function findCircleUsers(callback){
+      circleUsersString.forEach(function(userString){
+        User.findOne({ 'spotifyId': userString}, function(err, foundUser) {
           if (err) return done(err);
+          if (foundUser) {
+            circle.users.push(foundUser._id);
+          } else {
+            var newUser = new User({
+              spotifyId: req.body.spotifyId,
+              circles: []
+            });
+            // circle.users.push(user._id);
+            newUser.save(function(err, user) {
+              if (err) return done(err);
+            });
+          }
         });
-        console.log(foundUser);
+      });
+      circle.save(function(err, circle){
+        if (err) res.json(err);
+        res.json(circle);
+      });
+      callback(null, 'two');
+    },
+    function assignCircles(callback){
+      circle.users.forEach(function(user){
+        User.findOne({ '_id': user }, function(err, user) {
+          if (err) return done(err);
+          if (user) {
+            var foundUser = user;
+            foundUser.circles.push(circle);
+            // foundUser.save(function(err){
+            //   if (err) return done(err);
+            // });
+          }
+        });
+      });
+      callback(null, 'three');
+    },
+    function(err, results){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(results);
       }
-    });
-  });
+    }
+  ]);
 };
 
 var updateCircle = function(req, res) {
@@ -86,7 +88,6 @@ var destroyCircle = function(req, res) {
 };
 
 module.exports = {
-  addCircleUsers: addCircleUsers,
   indexCircle: indexCircle,
   createCircle: createCircle,
   updateCircle: updateCircle,
