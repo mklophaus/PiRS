72f7482009979c4c3f45202d0b9fb0e85cbe52da
J T <njthood@yahoo.com>
2015-11-29 21:58:04 -0800

    git heroku fixing

--- config/database.js
+++ config/database.js
@@ -1,10 +1,3 @@
-// var mongoose = require('mongoose');
-
-// require('dotenv').load()
-
-// mongoose.connect(process.env.DATABASE_URL);
-
-// module.exports = mongoose;
 
 
 var mongoose = require('mongoose');
@@ -12,17 +5,7 @@ var mongoose = require('mongoose');
 var env = require('./environment');
 
 // Use different database URIs based on whether an env var exists.
-<<<<<<< HEAD
-var dbUri = env.DATABASE_URL ||
-            'mongodb://localhost/' + env.SAFE_TITLE;
 
-if (!env.DATABASE_URL) {
-  // check that MongoD is running...
-  require('net').connect(27017, 'localhost').on('error', function() {
-    console.log("YOU MUST BOW BEFORE THE MONGOD FIRST, MORTAL!");
-    process.exit(0);
-  });
-=======
 var dbUri = env.MONGOLAB_URI ||
            'mongodb://localhost/' + env.SAFE_TITLE;
 
@@ -32,7 +15,6 @@ if (!env.MONGOLAB_URI) {
    console.log("YOU MUST BOW BEFORE THE MONGOD FIRST, MORTAL!");
    process.exit(0);
  });
->>>>>>> 29bc278413d2304eb0d164b3435e0adac2a17fdf
 }
 
 mongoose.connect(dbUri);
--- config/routes.js
+++ config/routes.js
@@ -65,7 +65,7 @@ var generateRandomString = function(length) {
 
 var stateKey = 'spotify_auth_state';
 
-<<<<<<< HEAD
+
   // router.get('/', function(req,res){
   //   res.render('index', { title: "WELCOME TO BOOMSQUAD!"});
 
@@ -86,23 +86,7 @@ var stateKey = 'spotify_auth_state';
       redirect_uri: 'https://piradio.herokuapp.com/callback',
       state: state
     }));
-=======
-router.get('/login', function(req, res) {
 
-var state = generateRandomString(16);
-res.cookie(stateKey, state);
-
-// your application requests authorization
-var scope = 'user-read-private user-read-email';
-res.redirect('https://accounts.spotify.com/authorize?' +
-  querystring.stringify({
-    response_type: 'code',
-    client_id: process.env.CLIENT_ID,
-    scope: scope,
-    redirect_uri: 'http://localhost:3000/callback',
-    state: state
-  }));
->>>>>>> 29bc278413d2304eb0d164b3435e0adac2a17fdf
 });
 
 router.get('/auth/spotify',
