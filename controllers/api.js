389be8a1f31e2dcc60df2b17839d83799fad2a74
Melissa Wilcox <melissa@emdubb.co>
2015-11-29 21:07:59 -0800

    add find circle method

--- controllers/api.js
+++ controllers/api.js
@@ -9,13 +9,25 @@ var indexCircle = function(req, res) {
   });
 };
 
+var findCircle = function(req, res) {
+  Circle.find({'_id': req.query._id }, function(err, record) {
+    res.json(record);
+  })
+}
+
 var indexUser = function(req, res) {
   User.find({}, function(err, records) {
     res.json(records);
   });
 };
 
+var displayCircleUsers = function(req, res) {
+
+}
+
 module.exports = {
   indexCircle: indexCircle,
-  indexUser: indexUser
+  findCircle: findCircle,
+  indexUser: indexUser,
+  displayCircleUsers: displayCircleUsers
 }
